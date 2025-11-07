7cf1f032d9c3defa0bdb2c1e49b3447399b2a8e9
ksw0518 <cs0503115@gmail.com>
2025-11-07 21:00:39 +0900

    bench 1388977

--- Laminar/Search.cpp
+++ Laminar/Search.cpp
@@ -579,6 +579,11 @@ inline int AlphaBeta(
                 continue;
             }
             int seeThreshold = isQuiet ? quietSEEMargin : noisySEEMargin;
+            if (isQuiet)
+            {
+                seeThreshold -= historyScore / 300;
+            }
+
             //if the Static Exchange Evaluation score is lower than certain margin,
             //assume the move is very bad and skip the move
             if (!SEE(board, move, seeThreshold))
