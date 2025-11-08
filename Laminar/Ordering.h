cf003a8551f9347455d3605f7b7d618deb7b2161
ksw0518 <cs0503115@gmail.com>
2025-11-08 11:46:56 +0900

    bench 1151488

--- Laminar/Ordering.h
+++ Laminar/Ordering.h
@@ -3,14 +3,22 @@
 #include "Movegen.h"
 #include "Search.h"
 #include "Transpositions.h"
-bool IsMoveCapture(Move& move);
-bool IsMoveQuiet(Move& move);
-bool IsMoveNoisy(Move& move);
-void SortMoves(MoveList& ml, Board& board, ThreadData& data, TranspositionEntry& entry, uint64_t threats);
-void SortNoisyMoves(MoveList& ml, Board& board, ThreadData& data);
-
 struct ScoredMove
 {
     int score;
     Move move;
 };
+void ChooseNextMove(ScoredMove* scored, MoveList& ml, int moveCount);
+bool IsMoveCapture(Move& move);
+bool IsMoveQuiet(Move& move);
+bool IsMoveNoisy(Move& move);
+void ScoreMoves(
+    ScoredMove* scored,
+    MoveList& ml,
+    Board& board,
+    ThreadData& data,
+    TranspositionEntry& entry,
+    uint64_t threats
+);
+void SortNoisyMoves(MoveList& ml, Board& board, ThreadData& data);
+
